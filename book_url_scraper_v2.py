c5bb83c4b0eccde7b0a30f11cf995aa9e8a186ca
aimwps <mwpskinner@gmail.com>
2021-04-12 15:20:58 +0100

    Added exceptions and a debugger for generating individual soup

--- book_url_scraper_v2.py
+++ book_url_scraper_v2.py
@@ -19,73 +19,109 @@ hundred_link_grabber("https://www.goodreads.com/list/show/1.Best_Books_Ever?page
 #############################################################################
 ## Functions for getting data from all books
 def get_author(page_soup):
-    author = page_soup.find('a', class_="authorName").get_text().strip()
-    ## Returns the author from the page
-    return author
-
-def get_title(page_soup):
-    title = page_soup.find('h1', id="bookTitle").get_text().strip()
-    return title
-
-def get_number_of_pages(page_soup):
-    number_of_pages_unclean = page_soup.find('span', itemprop="numberOfPages").get_text()
-    number_of_pages = int("".join([char for char in number_of_pages_unclean if char.isnumeric()]))
-    return number_of_pages
-
-def get_number_of_ratings(page_soup):
-    number_of_ratings_unclean = page_soup.find('meta', itemprop="ratingCount")
-    number_of_ratings = int(number_of_ratings_unclean['content'])
-    return number_of_ratings
-
-def get_first_published(page_soup):
-    details_section = page_soup.find('div', id="details")
-    first_published_unclean = details_section.find("nobr", class_="greyText")
-    if first_published_unclean != None:
-        text = first_published_unclean.get_text()
-        first_published = int(text.strip()[-6:-1])
-    else:
-        return None
-    return first_published
-
-def get_is_series(page_soup):
-    series_section = page_soup.find('h2', id="bookSeries")
-    if series_section!= None:
-        section_text = series_section.find('a').get_text()
-        if "#" in section_text:
-            return True
-        else:
-            return False
-    else:
+    try:
+        author = page_soup.find('a', class_="authorName").get_text().strip()
+        ## Returns the author from the page
+        return author
+    except:
         return np.nan
 
+def get_title(page_soup):
+    try:
+        title = page_soup.find('h1', id="bookTitle").get_text().strip()
+        return title
+    except:
+        return np.nan
+def get_number_of_pages(page_soup):
+    try:
+        number_of_pages_unclean = page_soup.find('span', itemprop="numberOfPages").get_text()
+        number_of_pages = int("".join([char for char in number_of_pages_unclean if char.isnumeric()]))
+        return number_of_pages
+    except:
+        return np.nan
+
+def get_number_of_ratings(page_soup):
+    try:
+        number_of_ratings_unclean = page_soup.find('meta', itemprop="ratingCount")
+        number_of_ratings = int(number_of_ratings_unclean['content'])
+        return number_of_ratings
+    except:
+        return np.nan
+
+def get_first_published(page_soup):
+    try:
+        details_section = page_soup.find('div', id="details")
+        first_published_unclean = details_section.find("nobr", class_="greyText")
+        if first_published_unclean != None:
+            text = first_published_unclean.get_text()
+            first_published = int(text.strip()[-6:-1])
+        else:
+            return None
+        return first_published
+    except:
+        return np.nan
+
+
+def get_is_series(page_soup):
+    try:
+        series_section = page_soup.find('h2', id="bookSeries")
+        print(series_section)
+        section_text = series_section.find('a')
+        if series_section != None:
+            check_if_series = series_section.find('a').get_text()
+            if "#" in check_if_series:
+                return True
+            else:
+                return False
+        else:
+            return np.nan
+    except:
+        return np.nan
+
+
 def get_awards(page_soup):
-    awards_section = page_soup.find('div', itemprop="awards")
-    awards = awards_section.find_all('a', class_="award")
-    main_awards = [award.get_text().strip() for award in awards]
-    str_main_awards = ", ".join(main_awards)
-    return str_main_awards
+    try:
+        awards_section = page_soup.find('div', itemprop="awards")
+        awards = awards_section.find_all('a', class_="award")
+        main_awards = [award.get_text().strip() for award in awards]
+        str_main_awards = ", ".join(main_awards)
+        return str_main_awards
+    except:
+        return np.nan
 
 def get_genres(page_soup):
-    genre_list_unclean = page_soup.find_all('a', class_="actionLinkLite bookPageGenreLink")
-    genre_list = [genre.get_text() for genre in genre_list_unclean]
-    str_genre_list = ", ".join(genre_list)
-    return str_genre_list
+    try:
+        genre_list_unclean = page_soup.find_all('a', class_="actionLinkLite bookPageGenreLink")
+        genre_list = [genre.get_text() for genre in genre_list_unclean]
+        str_genre_list = ", ".join(genre_list)
+        return str_genre_list
+    except:
+        return np.nan
 
 def get_place(page_soup):
-    get_place=page_soup.select('a[href*="/places"]')
-    place=[]
-    for x in get_place:
-        place.append(x.get_text())
-    return ", ".join(place)
+    try:
+        get_place=page_soup.select('a[href*="/places"]')
+        place=[]
+        for x in get_place:
+            place.append(x.get_text())
+        return ", ".join(place)
+    except:
+        return np.nan
 
 def get_num_reviews(page_soup):
-    get_num_unclean=page_soup.find('meta',itemprop="reviewCount")
-    get_num_reviews=int(get_num_unclean['content'])
-    return get_num_reviews
+    try:
+        get_num_unclean=page_soup.find('meta',itemprop="reviewCount")
+        get_num_reviews=int(get_num_unclean['content'])
+        return get_num_reviews
+    except:
+        return np.nan
 
 def get_avg(page_soup):
-    get_avg=float(page_soup.find('span',itemprop="ratingValue").get_text())
-    return get_avg
+    try:
+        get_avg=float(page_soup.find('span',itemprop="ratingValue").get_text())
+        return get_avg
+    except:
+        return np.nan
 
 def get_all_books(list_of_urls):
     pd_data =[]
@@ -125,6 +161,12 @@ def main_app():
     list_of_urls = hundred_link_grabber("https://www.goodreads.com/list/show/1.Best_Books_Ever?page=1")
     get_book_data = get_all_books(list_of_urls)
     return get_book_data
+
+
+def debugger_help(book_url):
+    request = requests.get(book_url)
+    page_soup = BeautifulSoup(request.content,'html.parser')
+    return page_soup
 
 x = main_app()
 print(len(x))
