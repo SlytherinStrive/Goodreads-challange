db81d44180e85567120c4db530f5d095375df946
aimwps <mwpskinner@gmail.com>
2021-04-12 14:13:39 +0100

    started work on main function

--- book_url_scraper.py
+++ book_url_scraper.py
@@ -11,6 +11,7 @@ def hundred_link_grabber(all_books_url):
     links_section = soup.find_all('a', class_="bookTitle", href=True)
     final_links = ["https://www.goodreads.com" + link['href'] for link in links_section]
     ## Returns a list of 100 book links
+    print(final_links)
     return final_links
 
 
@@ -19,14 +20,14 @@ def hundred_link_grabber(all_books_url):
 def get_author(page_url):
     request = requests.get(page_url)
     page_soup = BeautifulSoup(request.content, 'html.parser')
-    author = page_soup.find('a', class_="authorName").get_text()
+    author = page_soup.find('a', class_="authorName").get_text().strip()
     ## Returns the author from the page
     return author
 
 def get_title(page_url):
     request = requests.get(page_url)
     page_soup = BeautifulSoup(request.content, 'html.parser')
-    title = page_soup.find('h1', id="bookTitle").get_text()
+    title = page_soup.find('h1', id="bookTitle").get_text().strip()
     return title
 
 def get_number_of_pages(page_url):
@@ -47,8 +48,12 @@ def get_first_published(page_url):
     request = requests.get(page_url)
     page_soup = BeautifulSoup(request.content, 'html.parser')
     details_section = page_soup.find('div', id="details")
-    first_published_unclean = details_section.find("nobr", class_="greyText").get_text()
-    first_published = int(first_published_unclean.strip()[-6:-1])
+    first_published_unclean = details_section.find("nobr", class_="greyText")
+    if first_published_unclean != None:
+        text_first_published.get_text()
+        first_published = int(text_first_published.strip()[-6:-1])
+    else:
+        return None
     return first_published
 
 def get_is_series(page_url):
@@ -80,7 +85,43 @@ def get_genres(page_url):
 
 
 
+def get_all_books(list_of_urls):
+    pd_data =[]
+    for book_url in list_of_urls:
+        url = book_url
+        title = get_title(book_url)
+        author = get_author(book_url)
+        #num_reviews = get_num_reviews(book_url)
+        num_ratings = get_number_of_ratings(book_url)
+        #avg_rating = get_average_rating()
+        num_pages = get_number_of_pages(book_url)
+        original_publish_year = get_first_published(book_url)
+        series = get_is_series(book_url)
+        genres = get_genres(book_url)
+        awards = get_awards(book_url)
+        a_book = {
+            "url": url,
+            "title":title,
+            "author" :author,
+            "num_reviews" : num_ratings,
+            "num_ratings" : num_ratings,
+            "num_pages" : num_pages,
+            "original_publish_year" : original_publish_year,
+            "series" :series,
+            "genres" : genres,
+            "awards" : awards}
+        pd_data.append(a_book)
+    return pd_data
+
+def main_app():
+    list_of_urls = hundred_link_grabber("https://www.goodreads.com/list/show/1.Best_Books_Ever?page=1")
+    get_book_data = get_all_books(list_of_urls)
+    return get_book_data
 
 
-x = get_genres("https://www.goodreads.com/book/show/2657.To_Kill_a_Mockingbird")
-print(x)
+x = main_app()
+
+print(len(x))
+
+for book in x:
+    print(book)
