cfb29bd57e7aebf4dde1d84631b1545b0cab99a6
fbulbulov <farrukh.bulbulov@gmail.com>
2021-04-14 16:33:49 +0200

    Question 10 defining functions

--- data_analyzer.py
+++ data_analyzer.py
@@ -7,60 +7,61 @@ from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv("data/prepocessed_14th.csv")
 
-# 1. Scatter plot between Number of ratings and Number of Pages
-def pages_vs_ratings_scat(df):
-    plt.figure(figsize=(12,5))
-    plt.scatter(x='num_pages', y='num_ratings', data = df)
-    plt.title('Pages vs ratings ', fontsize='20')
-    plt.xlabel('Number of pages')
-    plt.ylabel('Number of ratings (in mln.)')
-    return plt.show()
-
-
 # 2. Correlation between Number of Reviews and Number of Pages
 corr_pages_review = df.corr().loc['num_pages','num_ratings']
 print (f"Correlation between number of pages and rating is {round(corr_pages_review,5)}")
 
-# 3. 3. Visualise the avg_rating distribution.
-def avg_rating_dist(df):
-    plt.figure(figsize=(12,5))
-    sns.displot(df, x="avg_rating", kde=True, fill=True)
-    plt.title('Average rating distribution', fontsize='20')
-    plt.xlabel('Average rating Distribution')
-    plt.show()
-
 # 4. Visualise the minmax_norm_rating distribution.
-sns.displot(df, x="minmax_norm_ratings", kde=True, fill=True)
-plt.title('MinMax Normalization distribution', fontsize='20')
-plt.xlabel('MinMax Distribution')
-plt.show()
+def min_max_norm(df):
+    sns.displot(df, x="minmax_norm_ratings", kde=True, fill=True)
+    plt.title('MinMax Normalization distribution', fontsize='13')
+    plt.xlabel('MinMax Distribution')
+    plt.show()
+min_max_norm(df)
 
 # 6. Create one graph that represents in the same figure both `minmax_norm_rating` and `mean_norm_rating`distributions.
-sns.histplot(data=df, x="normalise_mean", color="navy", label="Mean", kde=True)
-sns.histplot(data=df, x="minmax_norm_ratings", color="red", label="Min/Max", kde=True)
-plt.xlabel('Rating')
-plt.title('Min/Max and Mean Distributions', fontsize='20')
-plt.legend(loc=1)
-plt.show()
-
+def distributions(df):
+    sns.histplot(data=df, x="normalise_mean", color="navy", label="Mean", kde=True)
+    sns.histplot(data=df, x="minmax_norm_ratings", color="red", label="Min/Max", kde=True)
+    plt.xlabel('Rating')
+    plt.title('Min/Max and Mean Distributions', fontsize='13')
+    plt.legend(loc=1)
+    plt.show()
+distributions(df)
 #8. Visualize the `awards` distribution in a **boxplot** and **aggregated bars**. Decide which of these representations gives us more information and in which cases they should be used.
 # a. boxplot
-df[['award_count']].plot.box(vert=False, color='blue')
-plt.title('Number of Awards', fontsize='20')
-plt.show
+def box_awards(df):
+    df[['award_count']].plot.box(vert=False, color='blue')
+    plt.title('Number of Awards', fontsize='13')
+    plt.show
+def box_awards(df)
+
 # b.aggregate bars of total awards by books
 most_awards = df.sort_values(by='award_count', ascending=False)
 most_awards=  most_awards[['title','award_count']]
 most_awards = most_awards.head(20)
 
-plt.figure(figsize=(10,5))
-sns.barplot(y='title', x='award_count', data = most_awards, palette="Paired")
-plt.title('15 Most Awarded Books', fontsize='20')
-plt.xlabel('Number of Awards')
-plt.ylabel('Book Title')
-plt.show()
+def agg_awards(df):
+    plt.figure(figsize=(10,5))
+    sns.barplot(y='title', x='award_count', data = most_awards, palette="Paired")
+    plt.title('15 Most Awarded Books', fontsize='13')
+    plt.xlabel('Number of Awards')
+    plt.ylabel('Book Title')
+    plt.show()
+agg_awards(df)
 
 
+# 10. Make a scatterplot to represent  minmax_norm_ratings in function of the number of awards won by the book
+#- Is there another representation that displays this in a more clear manner?
+#- Optional: Can you plot a best fit linear regression line to represent the relationship?
+def cor_award_minmax (df):
+    sns.regplot(x = "award_count", y = "minmax_norm_ratings", line_kws={"color": "red"}, data = df)
+    plt.xlim(0,20)
+    plt.title('Correlation Between Awards Number and Min/Max Normalised Ratings', fontsize='12')
+    plt.xlabel('Number of Awards')
+    plt.ylabel('Min/Max Normalization')
+    plt.show()
+cor_award_minmax (df)
 
 # Graph most reviewed books
 def most_reviewed_books(df):
@@ -74,21 +75,50 @@ def most_reviewed_books(df):
     plt.ylabel('Book Title')
     return plt.show()
 
+def plotCorrelationMatrix(df, graphWidth):
+    filename = df
+    df = df[[col for col in df if df[col].nunique() > 1]] # keep columns where there are more than 1 unique values
+    if df.shape[1] < 2:
+        print(f'No correlation plots shown: The number of non-NaN or constant columns ({df.shape[1]}) is less than 2')
+        return
+    corr = df.corr()
+    plt.figure(num=None, figsize=(graphWidth, graphWidth), dpi=80, facecolor='w', edgecolor='k')
+    corrMat = plt.matshow(corr, fignum = 1)
+    plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
+    plt.yticks(range(len(corr.columns)), corr.columns)
+    plt.gca().xaxis.tick_bottom()
+    plt.colorbar(corrMat)
+    plt.title(f'Correlation Matrix for {filename}', fontsize=15)
+    return plt.show()
+
+def plotPerColumnDistribution(df, nGraphShown, nGraphPerRow):
+    nunique = df.nunique()
+    df = df[[col for col in df if nunique[col] > 1 and nunique[col] < 50]] # For displaying purposes, pick columns that have between 1 and 50 unique values
+    nRow, nCol = df.shape
+    columnNames = list(df)
+    nGraphRow = (nCol + nGraphPerRow - 1) / nGraphPerRow
+    plt.figure(num = None, figsize = (6 * nGraphPerRow, 8 * nGraphRow), dpi = 80, facecolor = 'w', edgecolor = 'k')
+    for i in range(min(nCol, nGraphShown)):
+        plt.subplot(nGraphRow, nGraphPerRow, i + 1)
+        columnDf = df.iloc[:, i]
+        if (not np.issubdtype(type(columnDf.iloc[0]), np.number)):
+            valueCounts = columnDf.value_counts()
+            valueCounts.plot.bar()
+        else:
+            columnDf.hist()
+        plt.ylabel('counts')
+        plt.xticks(rotation = 90)
+        plt.title(f'{columnNames[i]} (column {i})')
+    plt.tight_layout(pad = 1.0, w_pad = 1.0, h_pad = 1.0)
+    return plt.show()
+
 
 input_dict = {
-            #"1":,
-            "2": pages_vs_ratings_scat(df),
-            "3": avg_rating_dist(df),
-            #"4": ,
-            #"5": ,
-            #"6": ,
-            #"7": ,
-            #"8": ,
-            #"9": ,
-            #"10": ,
-            #"11": most_reviewd_books(df),
-
+            "1": most_reviewed_books(df),
+            "2": plotCorrelationMatrix(df, 8),
+            "3": plotPerColumnDistribution(df, 10, 5),
             }
+
 
 finished = False
 viewing = None
