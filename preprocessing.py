2bc7ec8de2ac22fc5206dc2d3c13fb2c03cd1443
aimwps <mwpskinner@gmail.com>
2021-04-14 12:10:43 +0100

    fixed error deleting some of preprocessing file. added 3000 more data rows

--- preprocessing.py
+++ preprocessing.py
@@ -31,7 +31,12 @@ def preprocessing(csv_lock):
     df[['url', 'title', 'author', 'genres', 'awards', 'place']] = df[['url', 'title', 'author', 'genres', 'awards', 'place']].astype(str)
     ### Add new columns
     df['normalise_mean'] = normalise_mean(df['avg_rating'])
-    
+
     print(df.isna().sum())
     print(df.info())
     return df.to_csv(f"data/preprocess_complete_.csv")
+
+
+if __name__ == "__main__":
+    load_csv = "data/scraped_2000.csv"
+    preprocessing(load_csv)
